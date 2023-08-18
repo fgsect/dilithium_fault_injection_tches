22b044203073983aa5ba583eb75b5214ef49c9e7
user.name <user.email>
2023-08-18 21:39:34 +0200

    try to use Dockerfile for POC

--- POC/chipwhisperer.dilithium/jupyter/courses/fault101/dilithium_solver/loop_abort_faults/run_with_gurobi_compute.sh
+++
@@ -1,2 +0,0 @@
-export PATH=$PATH:/home/users/vulitzsch/software/gurobi911/linux64/bin
-LD_LIBRARY_PATH=/home/users/vulitzsch/software/gurobi911/linux64/lib GUROBI_HOME=/home/users/vulitzsch/software/gurobi911/linux64/ sage $@ #solve_cs_gurobi.py $@
